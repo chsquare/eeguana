b8b22db21d7a3ee6abd979ce462be3dcb1cffb58
bnicenboim <bruno.nicenboim@gmail.com>
2019-04-01 14:45:39 +0200

    improve the way methods are handled

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,45 @@
-  # CRAN Note avoidance
-  if(getRversion() >= "2.15.1") 
+stop("not load dplyr and ggplot at the beg")
+
+.onLoad <- function(...) {
+    register_s3_method("dplyr", "group_by", "eeg_lst")
+    register_s3_method("dplyr", "filter", "eeg_lst")
+    register_s3_method("dplyr", "summarise", "eeg_lst")
+    register_s3_method("dplyr", "mutate", "eeg_lst")
+    register_s3_method("dplyr", "arrange", "eeg_lst")
+    register_s3_method("dplyr", "select", "eeg_lst")
+    register_s3_method("dplyr", "select", "ica_lst")
+    register_s3_method("dplyr", "rename", "eeg_lst")
+    register_s3_method("dplyr", "rename", "ica_lst")
+    register_s3_method("dplyr", "left_join", "eeg_lst")
+    invisible()
+}
+
+register_s3_method <- function(pkg, generic, class, fun = NULL) {
+    stopifnot(is.character(pkg), length(pkg) == 1)
+    stopifnot(is.character(generic), length(generic) == 1)
+    stopifnot(is.character(class), length(class) == 1)
+
+    if (is.null(fun)) {
+        fun <- get(paste0(generic, ".", class), envir = parent.frame())
+    } else {
+        stopifnot(is.function(fun))
+    }
+
+    if (pkg %in% loadedNamespaces()) {
+        registerS3method(generic, class, fun, envir = asNamespace(pkg))
+    }
+
+                                        # Always register hook in case package is later unloaded & reloaded
+    setHook(
+        packageEvent(pkg, "onLoad"),
+        function(...) {
+            registerS3method(generic, class, fun, envir = asNamespace(pkg))
+        }
+    )
+}
+                                        # nocov end
+
+if(getRversion() >= "2.15.1")
     utils::globalVariables(
 		c(".", unlist(obligatory_cols),
 		  "..cols","..cols_events","..cols_events_temp","..cols_signal",
